d718cc5ff7552e4246afe0172cfb8428d9190250
hiroi-p <nadaseanpaul@gmail.com>
2024-10-16 09:25:43 +0300

    Commit noaptea

--- Lab2/src/Exercitiu1/Main.java
+++ Lab2/src/Exercitiu1/Main.java
@@ -11,15 +11,22 @@ public class Main {
         Vector<String> v = new Vector<>(41,1);
         Scanner sc = new Scanner(new File("src/Exercitiu1/judete_in.txt"));
         Scanner afis = new Scanner(System.in);
+        String []array2 = new String[50];
+        int i=0;
         while(sc.hasNextLine()) {
-            v.add(sc.nextLine());
+
+           // v.add(sc.nextLine());
+            array2[i] = sc.nextLine();
+            i++;
         }
         String[] array = v.toArray(new String[0]);
         //Arrays.sort(array);
-        Arrays.sort(array, 0,40);
+        //Arrays.sort(array, 0,40);
+        Arrays.sort(array2, 0,40);
         sc.close();
         String a = afis.nextLine();
-        System.out.println(Arrays.binarySearch(array,a));
+        //System.out.println(Arrays.binarySearch(array,a));
+        System.out.println(Arrays.binarySearch(array2,a));
         }
     }
 
